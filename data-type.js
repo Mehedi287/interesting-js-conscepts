c51c46ab802a9620ca2ed619935df0e237d6cfb8
Mehedi390 <mh0816150@gmail.com>
2021-08-29 10:09:47 -0700

    data type

---
+++ data-type.js
@@ -0,0 +1,9 @@
+// this is a numbver type data 
+const a = 980;
+// console.log(typeof (a));
+// this is a string type data 
+const b = 'this is a string';
+console.log(typeof (b));
+// this is an boolean type data 
+const c = true;
+console.log(typeof (c));
