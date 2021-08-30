a049b00538834f39d3c7220c8d3dc99cd181e765
Mehedi390 <mh0816150@gmail.com>
2021-08-29 22:29:07 -0700

    double

---
+++ double-threepul.js
@@ -0,0 +1,24 @@
+//double equale == its convert the string  to integer and 
+//ture convert to 1 and false convert to 0
+
+const a = 4;
+const b = '4';
+const c = true;
+const d = 1;
+const e = 0;
+const f = false;
+if (e == f) {
+    console.log('thre are equale');
+}
+else {
+    console.log('thre are not equale');
+}
+//there pule ===' its check the type and the semililarity of tow value
+const v = 8;
+const x = '8';
+if (v === x) {
+    console.log('there are quale');
+}
+else {
+    console.log('there are not equale');
+}
